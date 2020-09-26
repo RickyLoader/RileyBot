601e77734273db5509a8c9e551f710da1075198d
Ricky <loaderricky123@gmail.com>
2020-09-27 01:21:25 +1200

    steak

--- src/main/java/Command/Commands/SteakCommand.java
+++ src/main/java/Command/Commands/SteakCommand.java
@@ -3,8 +3,10 @@ package Command.Commands;
 import Command.Structure.CommandContext;
 import Command.Structure.DiscordCommand;
 import Command.Structure.EmbedHelper;
-import Network.ImgurManager;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 
 import javax.imageio.ImageIO;
@@ -27,18 +29,19 @@ public class SteakCommand extends DiscordCommand {
     public void execute(CommandContext context) {
         MessageChannel channel = context.getMessageChannel();
         try {
-            BufferedImage fan = getUserAvatar(context.getUser());
-            if(fan == null) {
+            Member biggestFan = context.getMember();
+            BufferedImage fanImage = getUserAvatar(biggestFan.getUser());
+            if(fanImage == null) {
                 throw new Exception();
             }
             BufferedImage steak = ImageIO.read(new File("src/main/resources/LOL/steak.jpg"));
             Graphics g = steak.getGraphics();
-            g.drawImage(fan, 205, 50, null);
+            g.drawImage(fanImage, 205, 50, null);
             g.dispose();
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             ImageIO.write(steak, "jpg", out);
             out.close();
-            channel.sendMessage(EmbedHelper.getBlankChar()).addFile(out.toByteArray(), "image.jpg").queue();
+            sendMessage(channel, out.toByteArray(), biggestFan.getEffectiveName());
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -47,15 +50,35 @@ public class SteakCommand extends DiscordCommand {
     }
 
     /**
+     * Build the selfie message
+     *
+     * @param channel     Channel to send selfie to
+     * @param steakSelfie Selfie with the greatest League of Legends player in history
+     * @param name        Name of the lucky fan
+     */
+    private void sendMessage(MessageChannel channel, byte[] steakSelfie, String name) {
+        String steak = "https://i.imgur.com/KXnIE3C.png";
+        MessageEmbed message = new EmbedBuilder()
+                .setColor(EmbedHelper.getPurple())
+                .setThumbnail(steak)
+                .setTitle(name + " - Photo with Steak!")
+                .setDescription("The **greatest** League of Legends player in LCS history.")
+                .setFooter("I wanted a short name and steak is my favourite food - Steak", steak)
+                .setImage("attachment://steak.jpg")
+                .build();
+        channel.sendMessage(message).addFile(steakSelfie, "steak.jpg").queue();
+    }
+
+    /**
      * Get and resize the user's avatar to the correct size for replacing
      * the person's head beside him.
      *
-     * @param user User who is a big fan of steak
+     * @param fan User who is a big fan of steak
      * @return User avatar
      */
-    private BufferedImage getUserAvatar(User user) {
+    private BufferedImage getUserAvatar(User fan) {
         try {
-            URLConnection connection = new URL(user.getEffectiveAvatarUrl()).openConnection();
+            URLConnection connection = new URL(fan.getEffectiveAvatarUrl()).openConnection();
             connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             connection.connect();
             BufferedImage smallAvatar = ImageIO.read(connection.getInputStream());
