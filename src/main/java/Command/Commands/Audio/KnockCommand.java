3c4627871a674b4f5a82eec274705829a6ae73eb
Ricky <loaderricky123@gmail.com>
2020-12-13 03:21:31 +1300

    Alternate knocking audio between left and right

--- src/main/java/Command/Commands/Audio/KnockCommand.java
+++ src/main/java/Command/Commands/Audio/KnockCommand.java
@@ -4,6 +4,10 @@ import Command.Structure.CommandContext;
 import Command.Structure.DiscordCommand;
 
 public class KnockCommand extends DiscordCommand {
+    private final String leftKnock = "https://www.youtube.com/watch?v=KkH3N9w5Ook";
+    private final String rightKnock = "https://www.youtube.com/watch?v=cbuExw2EheQ";
+    private String lastKnock = leftKnock;
+
     public KnockCommand() {
         super("knock", "Knock knock");
     }
@@ -11,7 +15,10 @@ public class KnockCommand extends DiscordCommand {
     @Override
     public void execute(CommandContext context) {
         context.getMessage().delete().queue(
-                aVoid -> context.playAudio("https://www.youtube.com/watch?v=cbuExw2EheQ")
+                delete -> {
+                    lastKnock = lastKnock.equals(leftKnock) ? rightKnock : leftKnock;
+                    context.playAudio(lastKnock);
+                }
         );
     }
 }
