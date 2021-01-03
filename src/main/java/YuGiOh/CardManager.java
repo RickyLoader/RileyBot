3a91d4e767a8fe9505e3f6d543e0500b8f44c076
Ricky <loaderricky123@gmail.com>
2021-01-04 01:02:37 +1300

    Refactor

--- src/main/java/YuGiOh/CardManager.java
+++ src/main/java/YuGiOh/CardManager.java
@@ -6,9 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Locale;
 
 import static YuGiOh.CardType.*;
 
