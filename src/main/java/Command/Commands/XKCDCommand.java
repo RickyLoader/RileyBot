d9a96ab1fa998a55b3dfbd3c68d85834d93814b7
Ricky <loaderricky123@gmail.com>
2021-02-26 15:37:16 +1300

    XKCD pre-load publication details of all comics to allow searching by title. Use the API to supplement pre-loaded details.

--- src/main/java/Command/Commands/XKCDCommand.java
+++ src/main/java/Command/Commands/XKCDCommand.java
@@ -3,18 +3,25 @@ package Command.Commands;
 import Command.Structure.CommandContext;
 import Command.Structure.DiscordCommand;
 import Command.Structure.EmbedHelper;
+import Command.Structure.PageableTableEmbed;
 import Network.NetworkRequest;
 import Network.NetworkResponse;
 import XKCD.Comic;
+import XKCD.Comic.PublicationDetails;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
+import java.util.*;
 
 /**
  * View XKCD comics
@@ -22,26 +29,72 @@ import java.util.Random;
 public class XKCDCommand extends DiscordCommand {
     private final String BASE_URL = "https://xkcd.com/";
     private final Random random = new Random();
-    private int LATEST_COMIC_ISSUE;
+    private final String thumbnail = "https://i.imgur.com/gshCGA1.png", helpMessage = "Type: xkcd for help";
+    private ArrayList<PublicationDetails> archive;
+    private long archiveFetched;
 
     public XKCDCommand() {
-        super("xkcd\nxkcd [issue #/latest]", "Check out some XKCD comics!");
-        this.LATEST_COMIC_ISSUE = getLatestComic().getIssue();
+        super("xkcd\nxkcd [random/issue #/latest/search term]", "Check out some XKCD comics!");
+        updateArchive();
     }
 
     /**
-     * Fetch the latest XKCD comic
-     *
-     * @return Latest XKCD comic
+     * Update the archive of comic publication details
+     * if it has been an hour since the last update (or the archive is null)
      */
-    private Comic getLatestComic() {
-        return parseComicJSON(
-                new NetworkRequest(BASE_URL + "info.0.json", false).get().body
-        );
+    private void updateArchive() {
+        long currentTime = System.currentTimeMillis();
+        if(archive != null && currentTime - archiveFetched < 3600000) {
+            return;
+        }
+        this.archive = fetchComicArchive();
+        this.archiveFetched = System.currentTimeMillis();
     }
 
     /**
-     * Get an XKCD comic by the issue number
+     * Fetch the archive of XKCD comic publication details.
+     *
+     * @return List of XKCD comic publication details (in ascending order of issue #)
+     */
+    private ArrayList<PublicationDetails> fetchComicArchive() {
+        ArrayList<PublicationDetails> archive = new ArrayList<>();
+        Document archivePage = fetchComicArchivePage();
+        if(archivePage == null) {
+            return null;
+        }
+        Elements publications = archivePage.getElementById("middleContainer").getElementsByTag("a");
+        for(Element publication : publications) {
+            archive.add(
+                    new PublicationDetails(
+                            Integer.parseInt(publication.attr("href").replace("/", "")),
+                            parseDate(publication.attr("title")),
+                            publication.text()
+                    )
+            );
+        }
+        archive.sort(Comparator.comparingInt(PublicationDetails::getIssue));
+        return archive;
+    }
+
+    /**
+     * Get the HTML document of the XKCD comic archive page
+     *
+     * @return HTML document of XKCD comic archive page
+     */
+    private Document fetchComicArchivePage() {
+        try {
+            return Jsoup.connect(BASE_URL + "archive").get();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get an XKCD comic by the issue number.
+     * Use the API to retrieve the description and image.
+     * The archived publication details contain the remaining info required to build a comic.
      *
      * @param issue Comic issue to get
      * @return Comic
@@ -51,41 +104,24 @@ public class XKCDCommand extends DiscordCommand {
         if(response.code == 404) {
             return null;
         }
-        return parseComicJSON(response.body);
-    }
-
-    /**
-     * Parse a JSON String in to a Comic object
-     *
-     * @param json JSON to parse
-     * @return Comic object from JSON
-     */
-    private Comic parseComicJSON(String json) {
-        JSONObject comic = new JSONObject(json);
+        int index = issue < 404 ? issue - 1 : issue - 2; // Issue #404 doesn't exist
+        JSONObject comic = new JSONObject(response.body);
         return new Comic(
-                comic.getInt("num"),
+                archive.get(index),
                 comic.getString("img"),
-                comic.getString("title"),
-                comic.getString("alt"),
-                parseDate(
-                        comic.getString("day"),
-                        comic.getString("month"),
-                        comic.getString("year")
-                )
+                comic.getString("alt")
         );
     }
 
     /**
-     * Parse a date String into a Date object of dd/MM/yyyy
+     * Parse a date String into a Date object
      *
-     * @param day   Day of month - 01
-     * @param month Month of year - 08
-     * @param year  Year - 2020
+     * @param dateString Date String in format yyyy-MM-dd
      * @return Date object
      */
-    private Date parseDate(String day, String month, String year) {
+    private Date parseDate(String dateString) {
         try {
-            return new SimpleDateFormat("dd/MM/yyyy").parse(day + "/" + month + "/" + year);
+            return new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -96,27 +132,119 @@ public class XKCDCommand extends DiscordCommand {
     @Override
     public void execute(CommandContext context) {
         MessageChannel channel = context.getMessageChannel();
+        Member member = context.getMember();
         String message = context
                 .getLowerCaseMessage()
                 .replace("xkcd", "")
                 .trim();
 
-        Comic comic;
-        if(message.equals("latest")) {
-            comic = getLatestComic();
-            LATEST_COMIC_ISSUE = comic.getIssue();
+        if(message.isEmpty()) {
+            channel.sendMessage(getHelpNameCoded()).queue();
+            return;
         }
-        else {
-            comic = getComic(message.isEmpty() ? getRandomIssueNumber() : getQuantity(message));
+        channel.sendTyping().queue();
+        updateArchive();
+
+        if(archive == null) {
+            channel.sendMessage(
+                    member.getAsMention() + " I wasn't able to fetch the XKCD archive bro, try again!"
+            ).queue();
+            return;
+        }
+
+        Comic comic;
+        switch(message) {
+            case "latest":
+                comic = getComic(archive.size());
+                break;
+            case "random":
+                comic = getComic(getRandomIssueNumber());
+                break;
+            default:
+                int issueNumber = getQuantity(message);
+                if(issueNumber > 0) {
+                    comic = getComic(issueNumber);
+                }
+                else {
+                    PublicationDetails[] searchResults = searchComics(message);
+                    if(searchResults.length == 1) {
+                        comic = getComic(searchResults[0].getIssue());
+                    }
+                    else {
+                        showSearchResults(message, searchResults, context);
+                        return;
+                    }
+                }
         }
 
         if(comic == null) {
             channel.sendMessage(
-                    context.getMember().getAsMention() + " Couldn't find that one bro, settle down"
+                    member.getAsMention() + " Couldn't find that one bro, settle down"
             ).queue();
             return;
         }
         channel.sendMessage(buildComicEmbed(comic)).queue();
+    }
+
+    /**
+     * Show the XKCD comic search results for the given query.
+     * Display in a pageable embed message.
+     *
+     * @param query         Query used to find search results
+     * @param searchResults Search results found with query
+     * @param context       Command context for initialising pageable embed
+     */
+    private void showSearchResults(String query, PublicationDetails[] searchResults, CommandContext context) {
+        new PageableTableEmbed(
+                context,
+                Arrays.asList(searchResults),
+                thumbnail,
+                "XKCD Comic Search",
+                searchResults.length + " results found for: **" + query + "**",
+                helpMessage,
+                new String[]{
+                        "Issue #",
+                        "Title",
+                        "Date"
+                },
+                5,
+                searchResults.length == 0 ? EmbedHelper.RED : EmbedHelper.GREEN
+        ) {
+            @Override
+            public String[] getRowValues(int index, List<?> items, boolean defaultSort) {
+                PublicationDetails publicationDetails = (PublicationDetails) items.get(index);
+                return new String[]{
+                        "#" + publicationDetails.getIssue(),
+                        publicationDetails.getTitle(),
+                        publicationDetails.getFormattedDate()
+                };
+            }
+
+            @Override
+            public void sortItems(List<?> items, boolean defaultSort) {
+                items.sort((Comparator<Object>) (o1, o2) -> {
+                    int i1 = ((PublicationDetails) o1).getIssue();
+                    int i2 = ((PublicationDetails) o2).getIssue();
+                    if(defaultSort) {
+                        return i1 - i2;
+                    }
+                    return i2 - i1;
+                });
+            }
+        }.showMessage();
+    }
+
+    /**
+     * Search the archived publication details for a title containing
+     * the given query.
+     *
+     * @return Array of search results
+     */
+    private PublicationDetails[] searchComics(String query) {
+        return archive
+                .stream()
+                .filter(publicationDetails -> publicationDetails.getTitle().toLowerCase().contains(query))
+                .toArray(PublicationDetails[]::new);
     }
 
     /**
@@ -126,16 +254,18 @@ public class XKCDCommand extends DiscordCommand {
      * @return Message embed detailing comic
      */
     private MessageEmbed buildComicEmbed(Comic comic) {
-        String thumbnail = "https://i.imgur.com/gshCGA1.png";
+        PublicationDetails details = comic.getPublicationDetails();
         return new EmbedBuilder()
-                .setTitle("#" + comic.getIssue() + " - " + comic.getTitle())
+                .setTitle("#" + details.getIssue() + " - " + details.getTitle())
                 .setImage(comic.getImage())
-                .setDescription(comic.getDesc() + " \n\n" + EmbedHelper.embedURL("View", BASE_URL + "/" + comic.getIssue()))
+                .setDescription(
+                        comic.getDesc()
+                                + "\n\n"
+                                + EmbedHelper.embedURL("View", BASE_URL + "/" + details.getIssue())
+                )
                 .setThumbnail(thumbnail)
                 .setFooter(
-                        "Date: " + comic.getFormattedDate()
-                                + " | "
-                                + "Try " + getTrigger().replace("\n", " or "),
+                        "Date: " + details.getFormattedDate() + " | " + helpMessage,
                         thumbnail
                 )
                 .setColor(EmbedHelper.GREEN)
@@ -143,12 +273,14 @@ public class XKCDCommand extends DiscordCommand {
     }
 
     /**
-     * Get a random comic issue number between 1 and the most recently released issue (inclusive)
+     * Get a random comic issue number between 1 and the most recently released issue (inclusive).
+     * Return 405 if the rolled issue is 404 - Issue #404 doesn't exist
      *
      * @return Random comic issue number
      */
     private int getRandomIssueNumber() {
-        return 1 + random.nextInt(LATEST_COMIC_ISSUE);
+        int issue = 1 + random.nextInt(archive.size());
+        return issue == 404 ? 405 : issue;
     }
 
     @Override
--- src/main/java/XKCD/Comic.java
+++ src/main/java/XKCD/Comic.java
@@ -7,24 +7,29 @@ import java.util.Date;
  * XKCD comic
  */
 public class Comic {
-    private final int issue;
-    private final Date date;
-    private final String image, title, desc;
+    private final PublicationDetails publicationDetails;
+    private final String image, desc;
 
     /**
-     * Create a comic
+     * Create an XKCD comic
      *
-     * @param issue Comic issue number
-     * @param image URL to comic image
-     * @param title Title of comic
-     * @param date  Release date
+     * @param publicationDetails Comic publication details
+     * @param image              URL to comic image
+     * @param desc               Comic description
      */
-    public Comic(int issue, String image, String title, String desc, Date date) {
-        this.issue = issue;
+    public Comic(PublicationDetails publicationDetails, String image, String desc) {
+        this.publicationDetails = publicationDetails;
         this.image = image;
-        this.title = title;
         this.desc = desc;
-        this.date = date;
+    }
+
+    /**
+     * Get the publication details of the comic - title, publication date, and issue #
+     *
+     * @return Comic publication details
+     */
+    public PublicationDetails getPublicationDetails() {
+        return publicationDetails;
     }
 
     /**
@@ -37,38 +42,69 @@ public class Comic {
     }
 
     /**
-     * Get the release date of the comic formatted in a String
-     *
-     * @return Release date String
-     */
-    public String getFormattedDate() {
-        return new SimpleDateFormat("dd/MM/yyyy").format(date);
-    }
-
-    /**
-     * Get the title of the comic
-     *
-     * @return Comic title
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * Get the issue number of the comic
-     *
-     * @return Issue number
-     */
-    public int getIssue() {
-        return issue;
-    }
-
-    /**
      * Get the image URL of the comic
      *
      * @return Image URL
      */
     public String getImage() {
         return image;
+    }
+
+    /**
+     * Comic publication details - Date, issue #, and title
+     */
+    public static class PublicationDetails {
+        private final int issue;
+        private final Date date;
+        private final String title;
+
+        /**
+         * Create the comic publication details
+         *
+         * @param issue Comic issue number
+         * @param title Title of comic
+         * @param date  Publication date
+         */
+        public PublicationDetails(int issue, Date date, String title) {
+            this.issue = issue;
+            this.date = date;
+            this.title = title;
+        }
+
+        /**
+         * Get the publication date of the comic
+         *
+         * @return Comic publication date
+         */
+        public Date getDate() {
+            return date;
+        }
+
+        /**
+         * Get the publication date of the comic formatted in a String
+         *
+         * @return Publication date String
+         */
+        public String getFormattedDate() {
+            return new SimpleDateFormat("dd/MM/yyyy").format(date);
+        }
+
+        /**
+         * Get the title of the comic
+         *
+         * @return Comic title
+         */
+        public String getTitle() {
+            return title;
+        }
+
+        /**
+         * Get the issue number of the comic
+         *
+         * @return Issue number
+         */
+        public int getIssue() {
+            return issue;
+        }
     }
 }
