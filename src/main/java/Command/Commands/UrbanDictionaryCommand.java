c65961b17472d275306c444c96a0ecac8834ab84
Ricky <loaderricky123@gmail.com>
2021-05-02 14:38:14 +1200

    Pageable urban dictionary definitions

--- src/main/java/Command/Commands/UrbanDictionaryCommand.java
+++ src/main/java/Command/Commands/UrbanDictionaryCommand.java
@@ -1,55 +1,163 @@
 package Command.Commands;
 
-import Command.Structure.CommandContext;
-import Command.Structure.DiscordCommand;
+import Command.Structure.*;
 import UrbanDictionary.*;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.util.ArrayList;
 
 /**
  * Search or get random definitions from the Urban Dictionary
  */
-public class UrbanDictionaryCommand extends DiscordCommand {
-
-    private UrbanDictionary urbanDictionary;
+public class UrbanDictionaryCommand extends OnReadyDiscordCommand {
+    private final UrbanDictionary urbanDictionary;
+    private String upvote, downvote;
 
     public UrbanDictionaryCommand() {
-        super("urbandict\nurbandict [search term]", "Get cool definitions from Urban Dictionary");
+        super(
+                "urbandict",
+                "Get cool definitions from Urban Dictionary",
+                "urbandict [search term/random]"
+        );
+        this.urbanDictionary = new UrbanDictionary();
     }
 
     @Override
     public void execute(CommandContext context) {
-        String msg = context.getMessageContent();
+        String word = context.getMessageContent().substring(getTrigger().length()).trim();
         MessageChannel channel = context.getMessageChannel();
 
-        if(!msg.equals("urbandict") && !msg.startsWith("urbandict ")) {
+        if(word.isEmpty()) {
             channel.sendMessage(getHelpNameCoded()).queue();
             return;
         }
 
-        if(urbanDictionary == null) {
-            urbanDictionary = new UrbanDictionary(context.getEmoteHelper(), getHelpName().replaceAll("\n", " | "));
-        }
-
         new Thread(() -> {
-            Definition definition;
-            if(msg.equals("urbandict")) {
-                definition = urbanDictionary.getRandomDefinition();
-            }
-            else {
-                definition = urbanDictionary.searchDefinition(msg.replace("urbandict ",""));
-            }
-
-            if(definition == null) {
-                channel.sendMessage("No definitions were found!").queue();
+            channel.sendTyping().queue();
+            if(word.equalsIgnoreCase("random")) {
+                Definition definition = urbanDictionary.getRandomDefinition();
+                MessageEmbed definitionEmbed = addDefinitionToEmbed(
+                        getDefaultEmbedBuilder(),
+                        definition,
+                        "Urban Dictionary Random | " + definition.getTerm()
+                )
+                        .build();
+                channel.sendMessage(definitionEmbed).queue();
                 return;
             }
-            channel.sendMessage(urbanDictionary.getDefinitionEmbed(definition)).queue();
+            ArrayList<Definition> definitions = urbanDictionary.searchDefinition(word);
+            if(definitions.isEmpty()) {
+                channel.sendMessage(
+                        context.getMember().getAsMention() + " No definitions found for **" + word + "**!"
+                ).queue();
+                return;
+            }
+            showDefinitions(context, definitions, word);
         }).start();
+    }
+
+    /**
+     * Display the given definitions in a pageable message embed
+     *
+     * @param context     Command context
+     * @param definitions Definitions to display
+     * @param query       Query used to find definitions
+     */
+    private void showDefinitions(CommandContext context, ArrayList<Definition> definitions, String query) {
+        new CyclicalPageableEmbed(
+                context,
+                definitions,
+                1
+        ) {
+            @Override
+            public EmbedBuilder getEmbedBuilder(String pageDetails) {
+                return getDefaultEmbedBuilder();
+            }
+
+            @Override
+            public void displayItem(EmbedBuilder builder, int currentIndex) {
+                Definition definition = (Definition) getItems().get(currentIndex);
+                addDefinitionToEmbed(
+                        builder,
+                        definition,
+                        "Urban Dictionary | " + query + " | " + (currentIndex + 1) + "/" + getItems().size()
+                );
+            }
+
+            @Override
+            public boolean nonPagingEmoteAdded(Emote e) {
+                return false;
+            }
+        }.showMessage();
+    }
+
+    /**
+     * Add the given definition to the given embed builder.
+     * Display upvotes/downvotes & definition usage etc.
+     * Don't set title
+     *
+     * @param builder    Embed builder to add definition to
+     * @param definition Definition to add to the embed builder
+     * @return Embed builder with definition added
+     */
+    private EmbedBuilder addDefinitionToEmbed(EmbedBuilder builder, Definition definition, String title) {
+        return builder
+                .setTitle(title, definition.getUrl())
+                .setDescription(getEmbedDescription(definition))
+                .addField(upvote, String.valueOf(definition.getUpvote()), true)
+                .addField(downvote, String.valueOf(definition.getDownvote()), true);
+    }
+
+    /**
+     * Get the embed builder to use when displaying definitions
+     *
+     * @return Embed builder
+     */
+    public EmbedBuilder getDefaultEmbedBuilder() {
+        String icon = "https://i.imgur.com/vQXfaJX.png";
+        return new EmbedBuilder()
+                .setColor(EmbedHelper.URBAN_DICT_BLUE)
+                .setImage(EmbedHelper.SPACER_IMAGE)
+                .setThumbnail(icon)
+                .setFooter("Try: " + getHelpName(), icon);
+    }
+
+    /**
+     * Get the String description of the given definition to use in a message embed
+     *
+     * @param definition Definition to display
+     * @return String description of the given definition
+     */
+    private String getEmbedDescription(Definition definition) {
+        String description = "**Definition**\n\n"
+                + definition.getExplanation() + "\n\n";
+
+        if(definition.hasQuote()) {
+            description += "**Usage**\n\n" + definition.getQuote() + "\n\n";
+        }
+        return description + EmbedHelper.embedURL(
+                "- *"
+                        + definition.getAuthor()
+                        + ", "
+                        + definition.formatSubmitted()
+                        + "*",
+                definition.getUrl()
+        );
+    }
+
+    @Override
+    public void onReady(JDA jda, EmoteHelper emoteHelper) {
+        this.upvote = EmoteHelper.formatEmote(emoteHelper.getUpvote());
+        this.downvote = EmoteHelper.formatEmote(emoteHelper.getDownvote());
     }
 
     @Override
     public boolean matches(String query, Message message) {
-        return query.startsWith("urbandict");
+        return query.startsWith(getTrigger());
     }
 }
--- src/main/java/UrbanDictionary/UrbanDictionary.java
+++ src/main/java/UrbanDictionary/UrbanDictionary.java
@@ -1,16 +1,11 @@
 package UrbanDictionary;
 
-import Command.Structure.EmbedHelper;
-import Command.Structure.EmoteHelper;
 import Network.NetworkRequest;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -20,47 +15,6 @@ import static UrbanDictionary.Definition.parseSubmissionDate;
  * Wrap the urban dictionary API
  */
 public class UrbanDictionary {
-    private final String upvote, downvote, help;
-
-    /**
-     * Create the Urban Dictionary and set emotes
-     *
-     * @param emoteHelper Emote helper
-     */
-    public UrbanDictionary(EmoteHelper emoteHelper, String help) {
-        this.upvote = EmoteHelper.formatEmote(emoteHelper.getUpvote());
-        this.downvote = EmoteHelper.formatEmote(emoteHelper.getDownvote());
-        this.help = help;
-    }
-
-    /**
-     * Show the definition of a term
-     *
-     * @param definition Definition to display
-     * @return Embed displaying the definition
-     */
-    public MessageEmbed getDefinitionEmbed(Definition definition) {
-        String icon = "https://i.imgur.com/vQXfaJX.png";
-
-        StringBuilder description = new StringBuilder("**Definition**\n\n");
-        description.append(definition.getExplanation()).append("\n\n");
-        if(definition.hasQuote()) {
-            description.append("**Usage**\n\n").append(definition.getQuote()).append("\n\n");
-        }
-        description.append(EmbedHelper.embedURL("- *" + definition.getAuthor() + ", " + definition.formatSubmitted() + "*", definition.getUrl()));
-
-        return new EmbedBuilder()
-                .setTitle("Urban Dictionary: " + definition.getTerm())
-                .setDescription(description.toString())
-                .addField(upvote, String.valueOf(definition.getUpvote()), true)
-                .addField(downvote, String.valueOf(definition.getDownvote()), true)
-                .setFooter("Try: " + help, icon)
-                .setThumbnail(icon)
-                .setImage(EmbedHelper.SPACER_IMAGE)
-                .setColor(EmbedHelper.URBAN_DICT_BLUE)
-                .build();
-    }
-
     /**
      * Strip the embedded links from given String
      *
@@ -77,16 +31,18 @@ public class UrbanDictionary {
      * Search for a definition on the Urban Dictionary
      *
      * @param term Term to search for
-     * @return Search result or null
+     * @return List of definitions for the given search term
      */
-    public Definition searchDefinition(String term) {
-        List<Definition> definitions = definitionRequest("https://api.urbandictionary.com/v0/define?term=" + term);
-        if(definitions == null) {
-            return null;
-        }
-        definitions = definitions.stream().filter(definition -> definition.getTerm().equalsIgnoreCase(term)).collect(Collectors.toList());
+    public ArrayList<Definition> searchDefinition(String term) {
+        ArrayList<Definition> definitions = definitionRequest(
+                "https://api.urbandictionary.com/v0/define?term=" + term
+        );
+        definitions = definitions
+                .stream()
+                .filter(definition -> definition.getTerm().equalsIgnoreCase(term))
+                .collect(Collectors.toCollection(ArrayList::new));
         Collections.sort(definitions);
-        return definitions.isEmpty() ? null : definitions.get(0);
+        return definitions;
     }
 
     /**
@@ -96,29 +52,26 @@ public class UrbanDictionary {
      */
     public Definition getRandomDefinition() {
         ArrayList<Definition> definitions = definitionRequest("https://api.urbandictionary.com/v0/random");
-        return definitions == null ? null : definitions.get(new Random().nextInt(definitions.size()));
+        return definitions.get(new Random().nextInt(definitions.size()));
     }
 
     /**
      * Retrieve a list of definitions from the given URL
      *
      * @param url URL to urban dictionary API endpoint
-     * @return Random definition from provided results
+     * @return List of definitions
      */
     private ArrayList<Definition> definitionRequest(String url) {
+        ArrayList<Definition> definitions = new ArrayList<>();
         String json = new NetworkRequest(url, false).get().body;
         if(json == null) {
-            return null;
+            return definitions;
         }
         JSONArray results = new JSONObject(json).getJSONArray("list");
-
-        ArrayList<Definition> definitions = new ArrayList<>();
-
         for(int i = 0; i < results.length(); i++) {
             definitions.add(parseDefinition(results.getJSONObject(i)));
         }
-
-        return definitions.isEmpty() ? null : definitions;
+        return definitions;
     }
 
     /**
