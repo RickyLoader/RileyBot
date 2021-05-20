8e031dae46af9a8f3d7d3033328ae607c11e170a
Ricky <loaderricky123@gmail.com>
2021-05-21 00:57:58 +1200

    Refactor alpha vape command to use Vapo instance

---
+++ src/main/java/Command/Commands/AlphaVapeCommand.java
@@ -0,0 +1,31 @@
+package Command.Commands;
+
+import Command.Structure.CommandContext;
+import Command.Structure.DiscordCommand;
+import Command.Structure.VapeStoreCommand;
+import Vape.Product;
+import Vape.Vapo;
+
+/**
+ * View the amazing alpha vape
+ */
+public class AlphaVapeCommand extends DiscordCommand {
+    private static final long ALPHA_ID = 9757349896L;
+
+    public AlphaVapeCommand() {
+        super("alpha", "View the amazing alpha vape!");
+    }
+
+    @Override
+    public void execute(CommandContext context) {
+        Vapo vapo = Vapo.getInstance();
+        Product alphaVape = vapo.getProductById(ALPHA_ID);
+        if(alphaVape == null) {
+            context.getMessageChannel().sendMessage(
+                    context.getMember() + " I am so sorry, the **ALPHA VAPE** is nowhere to be found!"
+            ).queue();
+            return;
+        }
+        VapeStoreCommand.showProduct(context, alphaVape, vapo, "Try: " + getTrigger());
+    }
+}
