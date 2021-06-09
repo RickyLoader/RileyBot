f0348d4e82c196e94a238894ae968c57cd277cae
Ricky <loaderricky123@gmail.com>
2021-06-10 00:35:52 +1200

    Add trigger to MakeAChoiceCommand

--- src/main/java/Command/Commands/MakeAChoiceCommand.java
+++ src/main/java/Command/Commands/MakeAChoiceCommand.java
@@ -3,23 +3,29 @@ package Command.Commands;
 import Command.Structure.CommandContext;
 import Command.Structure.DiscordCommand;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 
 import java.util.Random;
 
 public class MakeAChoiceCommand extends DiscordCommand {
 
     public MakeAChoiceCommand() {
-        super("[option] or [option] or [option]...", "Choose between multiple things!");
+        super("or", "Choose between multiple things!", "[option] or [option] or [option]...");
     }
 
     @Override
     public void execute(CommandContext context) {
-        String[] options = context.getLowerCaseMessage().split(" or ");
-        context.getMessageChannel().sendMessage(options[new Random().nextInt(options.length)]).queue();
+        MessageChannel channel = context.getMessageChannel();
+        String[] options = context.getLowerCaseMessage().split(" " + getTrigger() + " ");
+        if(options.length < 2) {
+            channel.sendMessage(getHelpNameCoded()).queue();
+            return;
+        }
+        channel.sendMessage(options[new Random().nextInt(options.length)]).queue();
     }
 
     @Override
     public boolean matches(String query, Message message) {
-        return query.split(" or ").length >= 2;
+        return query.contains(getTrigger());
     }
 }
