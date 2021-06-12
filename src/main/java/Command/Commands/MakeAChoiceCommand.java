3471d357633de144dc3c71a5ffbd097a23faacde
Ricky <loaderricky123@gmail.com>
2021-06-12 15:51:16 +1200

    Fix calling help

--- src/main/java/Command/Commands/MakeAChoiceCommand.java
+++ src/main/java/Command/Commands/MakeAChoiceCommand.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import java.util.Random;
 
 public class MakeAChoiceCommand extends DiscordCommand {
+    private static final int MIN_OPTIONS = 2;
 
     public MakeAChoiceCommand() {
         super("or", "Choose between multiple things!", "[option] or [option] or [option]...");
@@ -16,16 +17,27 @@ public class MakeAChoiceCommand extends DiscordCommand {
     @Override
     public void execute(CommandContext context) {
         MessageChannel channel = context.getMessageChannel();
-        String[] options = context.getLowerCaseMessage().split(" " + getTrigger() + " ");
-        if(options.length < 2) {
+        String query = context.getLowerCaseMessage();
+        if(query.equals(getTrigger())) {
             channel.sendMessage(getHelpNameCoded()).queue();
             return;
         }
+        String[] options = getOptions(query);
         channel.sendMessage(options[new Random().nextInt(options.length)]).queue();
+    }
+
+    /**
+     * Split the given String on occurrence of the trigger in to an array of options
+     *
+     * @param query Query to split
+     * @return Array of options
+     */
+    public String[] getOptions(String query) {
+        return query.split(" " + getTrigger() + " ");
     }
 
     @Override
     public boolean matches(String query, Message message) {
-        return query.contains(getTrigger());
+        return query.equals(getTrigger()) || getOptions(query).length >= MIN_OPTIONS;
     }
 }
