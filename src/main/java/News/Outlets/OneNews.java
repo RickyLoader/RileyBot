3bee5c378567fbac3ac3a4af2c5852d3d3df6a33
Ricky <loaderricky123@gmail.com>
2021-11-07 14:21:26 +1300

    Update One News domain & fix date

--- src/main/java/News/Outlets/OneNews.java
+++ src/main/java/News/Outlets/OneNews.java
@@ -13,15 +13,22 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * One News article parsing using their mobile app API
  */
 public class OneNews extends NewsOutlet {
     private static final String
-            BASE_URL = "https://www.tvnz.co.nz/",
-            BASE_ARTICLE_URL = BASE_URL + "one-news/",
+            OLD_DOMAIN = "https://www.tvnz.co.nz/",
+            ARTICLE_REGEX = "(.+)/(.+)(/)?(.+)?",
+            OLD_ARTICLE_REGEX = OLD_DOMAIN + "one-news/" + ARTICLE_REGEX,
+            NEW_ARTICLE_REGEX = "https://www.1news.co.nz/" + ARTICLE_REGEX,
             API_URL = "https://api.tvnz.co.nz/api/content/tvnz/news/story.androidphone.v3.json/";
 
     /**
@@ -31,7 +38,7 @@ public class OneNews extends NewsOutlet {
         super(
                 "One News",
                 "https://i.imgur.com/4Gp3qnH.png",
-                BASE_ARTICLE_URL + "(.+)/(.+)(/)?(.+)?",
+                OLD_ARTICLE_REGEX,
                 EmbedHelper.RED
         );
     }
@@ -69,7 +76,6 @@ public class OneNews extends NewsOutlet {
             );
         }
         catch(Exception e) {
-            e.printStackTrace();
             return null;
         }
     }
@@ -135,7 +141,7 @@ public class OneNews extends NewsOutlet {
                     new Image(
 
                             // Given as "/path/to/image" -> remove first slash to append to base URL
-                            BASE_URL + target.getString(imageKey).replaceFirst("/", ""),
+                            OLD_DOMAIN + target.getString(imageKey).replaceFirst("/", ""),
                             target.has(captionKey) ? target.getString(captionKey) : null
                     )
             );
@@ -147,16 +153,51 @@ public class OneNews extends NewsOutlet {
      * Make a request to the given article URL and attempt to scrape the relative mobile URL path from the HTML.
      * This is the parameter required by {@link #getArticleDataByRelativeMobilePath(String)} to retrieve data about an
      * article.
-     * It is found in a meta tag that is used to open an article within the IOS app and is in the
+     * It may be found in one of two places:
+     * 1. A meta tag that is used to open an article within the mobile app when viewing from a mobile browser, in the
      * format "app-id={APP_ID}, app-argument=tvnz-one-news://story/{RELATIVE_PATH}"
+     * 2. Embedded JSON within a script tag with the ID: "fusion-metadata" .
      *
      * @param articleUrl URL to article - e.g "https://www.tvnz.co.nz/one-news/{CATEGORY}/{TITLE_SLUG}"
-     * @return Relative mobile path or null (if unable to locate) e.g "content/onenews/story/{CATEGORY}/{TITLE_SLUG}"
+     * @return Relative mobile path or null (if unable to locate) e.g "content/tvnz/onenews/story/{CATEGORY}/{TITLE_SLUG}"
      */
     @Nullable
     private String getRelativeMobilePath(String articleUrl) {
         try {
-            Document articleDocument = Jsoup.connect(articleUrl).get();
+            Document document = Jsoup.connect(articleUrl).get();
+
+            String relativeMobilePathByMetaTag = getRelativeMobilePathByMetaTag(document);
+
+            return relativeMobilePathByMetaTag == null
+                    ? getRelativeMobilePathByScriptTag(document)
+                    : relativeMobilePathByMetaTag;
+        }
+        catch(Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Override as One News has changed their domain from "www.tvnz.co.nz" to "www.1news.co.nz", the old domain still
+     * works but redirects to the new domain.
+     */
+    @Override
+    public boolean isNewsUrl(String query) {
+        return query.matches(OLD_ARTICLE_REGEX) || query.matches(NEW_ARTICLE_REGEX);
+    }
+
+    /**
+     * Attempt to scrape the relative mobile URL path from the HTML of an article.
+     * Do this by locating a meta tag that is used to open an article within the mobile app when viewing from a
+     * mobile browser.
+     * This is in the format "app-id={APP_ID}, app-argument=tvnz-one-news://story/{RELATIVE_PATH}"
+     *
+     * @param articleDocument HTML document of the article
+     * @return Relative mobile path or null (if unable to locate) e.g "content/tvnz/onenews/story/{CATEGORY}/{TITLE_SLUG}"
+     */
+    @Nullable
+    private String getRelativeMobilePathByMetaTag(Document articleDocument) {
+        try {
 
             // "app-id={APP_ID}, app-argument=tvnz-one-news://story/{RELATIVE_PATH}"
             final String mobileArguments = HTMLUtils.getMetaTagValue(
@@ -180,9 +221,46 @@ public class OneNews extends NewsOutlet {
     }
 
     /**
+     * Attempt to scrape the relative mobile URL path from the HTML of an article.
+     * Do this by locating some embedded JSON within a script tag with the ID: "fusion-metadata".
+     * This is in the format "source_id":"content/tvnz/onenews/story/{CATEGORY}/{TITLE_SLUG}"
+     *
+     * @param articleDocument HTML document of the article
+     * @return Relative mobile path or null (if unable to locate) e.g "content/tvnz/onenews/story/{CATEGORY}/{TITLE_SLUG}"
+     */
+    @Nullable
+    private String getRelativeMobilePathByScriptTag(Document articleDocument) {
+        try {
+
+            // Contains key-value pairs in the format "Fusion.x;Fusion.y;Fusion.z;" - one of these is content JSON
+            String scriptContent = articleDocument.getElementById("fusion-metadata").html();
+
+            /*
+             * Matches everything between "Fusion.globalContent=" and the first encountered ";" that is preceded by "}".
+             * This should be a JSON overview of the article content.
+             */
+            Pattern pattern = Pattern.compile("(?<=Fusion.globalContent=)[\\s\\S]+?}(?=;)");
+
+            Matcher matcher = pattern.matcher(scriptContent);
+
+            // Content JSON not found in script tag
+            if(!matcher.find()) {
+                throw new Exception("JSON content not found for article.");
+            }
+
+            JSONObject content = new JSONObject(scriptContent.substring(matcher.start(), matcher.end()));
+
+            return content.getJSONObject("source").getString("source_id");
+        }
+        catch(Exception e) {
+            return null;
+        }
+    }
+
+    /**
      * Get the URL required to fetch data for an article with the given relative mobile path.
      *
-     * @param relativeMobilePath Relative path to article in mobile app - e.g "content/onenews/story/{CATEGORY}/{TITLE_SLUG}"
+     * @param relativeMobilePath Relative path to article in mobile app - e.g "content/tvnz/onenews/story/{CATEGORY}/{TITLE_SLUG}"
      * @return URL to fetch article data for given path
      */
     private String getArticleDataUrl(String relativeMobilePath) {
@@ -196,7 +274,7 @@ public class OneNews extends NewsOutlet {
      * Relative web: /one-news/new-zealand/dunedin-farmer-protest-gets-heated-woman-has-sign-ripped-her-hands
      * Relative mobile: /content/tvnz/onenews/story/2021/07/16/dunedin-farmers-protest-gets-heated-as-environmental-supporter-h
      *
-     * @param relativeMobilePath Relative path to article in mobile app - e.g "content/onenews/story/{CATEGORY}/{TITLE_SLUG}"
+     * @param relativeMobilePath Relative path to article in mobile app - e.g "content/tvnz/onenews/story/{CATEGORY}/{TITLE_SLUG}"
      * @return Article JSON data or null (if unable to retrieve)
      */
     @Nullable
@@ -207,6 +285,17 @@ public class OneNews extends NewsOutlet {
         if(response.code != 200) {
             return null;
         }
+
         return new JSONObject(response.body);
+    }
+
+    @Override
+    public Date parseDate(String dateString) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse(dateString);
+        }
+        catch(ParseException e) {
+            return new Date();
+        }
     }
 }
